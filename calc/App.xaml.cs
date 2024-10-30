19768b80a5c2965cf891ea57645dca472a502872
spooky <zxoocy@gmail.com>
2024-10-31 00:19:58 +0700

    Initialize bug

--- calc/App.xaml.cs
+++ calc/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Configuration;
 using System.Data;
 using System.Windows;
-using WpfCalculator;
+using calc;
 
 namespace calc
 {
--- calc/obj/Debug/net8.0-windows/calc.AssemblyInfo.cs
+++ calc/obj/Debug/net8.0-windows/calc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe4673f8ad0b75733732e5db464c1c10861cd6a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c35665a5f676b554d315eb0a46e8a43846d5409")]
 [assembly: System.Reflection.AssemblyProductAttribute("calc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
